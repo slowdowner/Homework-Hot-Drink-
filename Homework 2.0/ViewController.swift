5aaf46453226e361fd0c6e57d746d9910ad807aa
MegaMount <megamount@megamountdemacbook-air.local>
2018-11-08 23:27:11 +0800

    Initial Commit

---
+++ Homework 2.0/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Homework 2.0
+//
+//  Created by MegaMount on 2018/11/8.
+//  Copyright © 2018年 MegaMount. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
